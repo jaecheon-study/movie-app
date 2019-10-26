9b70e0b3b652c8ed7e815a1c408449714e0950a5
jaecheon <kzc0222@gmail.com>
2019-10-26 10:48:03 +0900

    add react init
    리엑트 기본 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  //  retrun은 html이 들어가야함.
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Test</h1>
     </div>
   );
 }
