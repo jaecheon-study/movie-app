1965e8cf6312d0ff1ef4f573573ff28527b50e6f
jaecheon <kzc0222@gmail.com>
2019-10-26 12:44:15 +0900

    add get movie Data
    영화 정보 api json 데이터 불러오기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types'
 // 네트워크 관련 라이브러리. (웹 플레폼에서 많이 사용되는 라이브러리)
 import axios from "axios";
+import Movie from './Movie'
 
 // 클래스 컴포넌트
 export default class App extends React.Component {
@@ -15,9 +16,14 @@ export default class App extends React.Component {
 
     getMovies = async () => {
         // get 방식으로 해당 url 불러옴
-        const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    }
-
+        // const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+        const {
+            data: {
+                data: { movies }
+            }
+        } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+        this.setState({movies, isLoading: false});
+    };
 
     // react LifeCycle
     // 페이지가 시작 될 때
@@ -33,12 +39,23 @@ export default class App extends React.Component {
     render() {
         // 상수 선언 부분 또는 변수 return을 만나기전
 
-        const {isLoading} = this.state;
+        const {movies, isLoading} = this.state;
 
         // return은 화면에 뿌려주는 역할
         return (
             <div>
-            { isLoading ? "Loading..." : "We are ready"}
+            { isLoading ? "Loading..."
+                    : movies.map( movie => (
+                        <Movie
+                            key={movie.id}
+                            id = {movie.id}
+                            year={movie.year}
+                            title={movie.title}
+                            summary={movie.summary}
+                            poster={movie.small_cover_image}
+                        />
+                    ))
+                }
             </div>
         );
     }
---
+++ src/Movie.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Movie({id, year, title, summary, poster}) {
+    return (
+        <div>
+            <h1>{id}</h1>
+            <h4>{title}</h4>
+            <h4>{year}</h4>
+            <h4>{summary}</h4>
+            <img src={poster}/>
+        </div>
+    )
+}
+
+Movie.prototype = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+};
+
+export default Movie;
