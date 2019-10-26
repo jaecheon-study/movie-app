6d50c500b54b94a8d8075b1ef75026c34f8abdee
jaecheon <kzc0222@gmail.com>
2019-10-26 12:13:57 +0900

    add planning the Movie Component
    기본 컴포넌트

--- src/App.js
+++ src/App.js
@@ -7,55 +7,28 @@ export default class App extends React.Component {
     // render가 실행전 상태 값, 상태 선언, 함수등이 들어감. (코드)
     // 상태 값은 state로 지정 되어야 한다. 다름 네이밍이면 에러
     state = { // 상태 선언 (초기 값)
-        count: 0
+        movies: [],
+        isLoading: true
     };
-    add = () => { // 함수
-      console.log("add");
-      // setState 다시 설정
-      this.setState( current => ({
-          count: current.count + 1
-      }));
-    };
-    minus = () => { // 함수
-      console.log("minus");
-        this.setState( current => ({
-            count: current.count - 1
-        }));
-      
-    };
-
-    // shouldComponentUpdate(nextProps, nextState, nextContext) {
-    // }
 
     // react LifeCycle
     // 페이지가 시작 될 때
     componentDidMount() {
-        console.log("componentDidMount");
-    }
-    
-    // 상태가 업데이트 될 때
-    componentDidUpdate() {
-        console.log("componentDidUpdate");
-    }
-    
-    // 화면이 종료 될 때 즉 페이지가 종료 될 때
-    componentWillUnmount() {
-        console.log("componentWillUnmount");
-    }
-    
-    // 에러 발생시
-    componentDidCatch() {
-        console.log("componentDidCatch");
+        setTimeout(() => {
+            this.setState({isLoading: false});
+        }, 6000)
     }
 
+    // 렌더링 부분
     render() {
         // 상수 선언 부분 또는 변수 return을 만나기전
+
+        const {isLoading} = this.state;
+
         // return은 화면에 뿌려주는 역할
         return (
             <div>
-                <h1>The number is: {this.state.count}</h1>
-                <button onClick={this.add}>add</button>
-                <button onClick={this.minus}>minus</button>
+            { isLoading ? "Loading..." : "We are ready"}
             </div>
         );
     }
